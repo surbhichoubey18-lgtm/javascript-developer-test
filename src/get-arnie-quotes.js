b71e7d42222df16062e43359eafda40e527c0b5d
surbhi2007 <surbhichoubey9@gmail.com>
2025-08-27 02:06:23 +0530

    feat: added getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,14 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const quotePromises= urls.map(async (url) => {
+    const response = await httpGet(url);
+    return { 
+      [response.status === 200 ? 'Arnie Quote' : 'FAILURE']: JSON.parse(response.body).message 
+    };
+  });
+
+  return Promise.all(quotePromises);
 };
 
 module.exports = {
